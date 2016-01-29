3cdd4f2ae5723537d45ec46b97bd5502094ab28d
LibertysYarn <code@libertysyarn.com>
2016-01-29 17:24:53 -0500

    working on titles

--- js/index.js
+++ js/index.js
@@ -1,77 +1,66 @@
-$(document).ready(function() {
-  getNews();
-  var $title = $('.title')
-  var $timePosted = $('.time')
-  var $link = $('.link')
-  var $metaDescription = $('.meta')
-  var $rank = $('.rank')
-  var $upVotes = $('.upvotes')
-  var $userId = $('.userId')
-  var $img = $('.title-img');
+$(function ($) {
+    $(document).ready(function () {
+      var $title = $('.title')
+      var $timePosted = $('.time')
+      var $link = $('.link')
+      var $metaDescription = $('.meta')
+      var $rank = $('.rank')
+      var $upVotes = $('.upvotes')
+      var $userId = $('.userId')
+      var $tileImage = $('.tile-img')
 
-  function getNews() {
-    $.get('http://www.freecodecamp.com/news/hot', function(hotNews) {
-      var title = hotNews.headline;
-      var link = hotNews.link;
-      var time = hotNews.timePosted;
-      var rank = hotNews.rank;
-      var upvotes = hotNews.upvotes.length;
-      var author = hotNews.author;
-      var userId = hotNews.userId;
-      var img = hotNews.image;
-      var meta = hotNews.metaDescription;
+      $(function getNews () {
+        $.get('http://www.freecodecamp.com/news/hot', function (news) {
+          // var title = news.headline
+          // var link = news.link
+          // var time = news.timePosted
+          // var rank = news.rank
+          // var upvotes = news.upvotes
+          // var author = news.author
+          // var userId = news.userId
+          // var tileImage = news.image
+          // var meta = news.metaDescription
 
-      $title.text(title);
-      $link.text(link);
-      $img.append("<img src="img">");
-      console.log(title);
-
-
-    }, 'jsonp')
-  }
+          $title.text(news.headline)
+          $link.append('href="' + news.link + '"')
+          $tileImage.append('<img src=' + news.image + '>')
+          console.log(news.headline)
+        })
+      })
 
 
 
+    $('.ripple-effect').click(function (e) {
+      var rippler = $(this)
 
+      // create .ink element if it doesn't exist
+      if (rippler.find('.ink').length === 0) {
+        rippler.append("<span class='ink'></span>")
+      }
 
-)
-})
+      var ink = rippler.find('.ink')
 
+      // prevent quick double clicks
+      ink.removeClass('animate')
 
-(function($) {
-  $(".ripple-effect").click(function(e) {
-    var rippler = $(this);
+      // set .ink diametr
+      if (!ink.height() && !ink.width()) {
+        var d = Math.max(rippler.outerWidth(), rippler.outerHeight())
+        ink.css({
+          height: d,
+          width: d
+        })
+      }
 
-    // create .ink element if it doesn't exist
-    if (rippler.find(".ink").length == 0) {
-      rippler.append("<span class='ink'></span>");
-    }
+      // get click coordinates
+      var x = e.pageX - rippler.offset().left - ink.width() / 2
+      var y = e.pageY - rippler.offset().top - ink.height() / 2
 
-    var ink = rippler.find(".ink");
-
-    // prevent quick double clicks
-    ink.removeClass("animate");
-
-    // set .ink diametr
-    if (!ink.height() && !ink.width()) {
-      var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
+      // set .ink position and add class .animate
       ink.css({
-        height: d,
-        width: d
-      });
-    }
-
-    // get click coordinates
-    var x = e.pageX - rippler.offset().left - ink.width() / 2;
-    var y = e.pageY - rippler.offset().top - ink.height() / 2;
-
-    // set .ink position and add class .animate
-    ink.css({
-      top: y + 'px',
-      left: x + 'px'
-    }).addClass("animate");
-  })
-})(jQuery);
-
-
-}
+        top: y + 'px',
+        left: x + 'px'
+      }).addClass('animate')
+    })
+    })
+})
