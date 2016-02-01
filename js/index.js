03d191502bc922d21f97e6f7d9854e8f4f359381
libertysyarn <code@libertysyarn.com>
2016-02-01 17:28:07 -0500

    working on author only list

--- js/index.js
+++ js/index.js
@@ -5,41 +5,37 @@ xhr.open("GET", "http://www.freecodecamp.com/news/hot");
 xhr.send();
 
 function reqListener() {
-  console.log(xhr.response[1].link);
-
   if (xhr.status === 200) {
     news = xhr.response;
-
     var newContent = '';
     for (var i = 0; i < news.length; i++) {
-
 
       newContent += '<div class="mdl-card mdl-cell mdl-cell--4" >';
       newContent += '<div class="mdl-card__media mdl-color-text--grey-50" style="background-image: url(' + news[i].image + ');">';
       newContent += '</div>';
       newContent += '<h3><a href="' + news[i].link + '">' + news[i].headline + '</a></h3>';
       newContent += '<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">';
-      newContent += '<div class="minilogo"><a href="http://www.freecodecamp.com/' + news[i].author.username + '"><img class="avatar" src="' + news[i].author.picture + ')"></a></div>';
+      newContent += '<div class="minilogo" data-value="'+ news[i].author.username +'"><a href=""><img class="avatar" src="' + news[i].author.picture + ')"></a></div>';
       newContent += '<div>';
-
       newContent += '<a class="author-link" href="http://www.freecodecamp.com/' + news[i].author.username + '"><strong>@' + news[i].author.username + '</strong></a>';
       newContent += '<span>' + jQuery.timeago(news[i].timePosted) + '</span>';
-      newContent += '<span>' + news[i].rank + (news[i].rank > 1 ? " points" : " point") + '</span></div>';
-      newContent += '<button class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--primary mdl-upvote-btn"><i class="material-icons mdl-color-text--white" role="presentation">arrow_upward</i><span class="visuallyhidden">arrow_upward</span></button>';
+      newContent += '<span>' + news[i].rank + (news[i].rank > 1 ? " votes" : " vote") + '</span></div>';
+      newContent += '<button id="' + news[i].id + '" class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--primary mdl-upvote-btn"><i class="material-icons mdl-color-text--white btn" role="presentation">arrow_upward</i><span class="visuallyhidden">arrow_upward</span></button>';
       newContent += '</div>';
       newContent += '</div>';
       newContent += '</div>';
       newContent += '</div>';
-
-
     }
-    var content = document.getElementById('news');
+    content = document.getElementById('news');
     content.innerHTML = newContent;
   }
-
+  //  else {
+  //   var error = '<h1>"NO NEWS FOR YOU!"</h1>';
+  //   content.innerHTML = error;
+  // }
 }
 
-
+//http://www.freecodecamp.com/' + news[i].author.username + '
 //ToDo
 // -make default background image if none exists
 // -assign colors of tiles to key words in title
@@ -51,18 +47,41 @@ function reqListener() {
 
 
 $.ready(function() {
-  var div = document.createElement('div');
-  var alreadyUpvoted = false;
 
-  $(div).find('.mdl-upvote-btn').each(function(idx, btn) {
-    var $btn = $(btn);
-    if (alreadyUpvoted) {
-      $btn.addClass('disabled');
-      $btn.text('upvoted!');
-    }
-    $btn.data('upVotes', news[i].upVotes);
+
+  $(".minilogo").on("click", function() {  
+    // var data = $(".minilogo").data("data-value");
+    // console.log(data);
+    $.getJSON("http://www.freecodecamp.com/news/hot", function(json) {
+
+      var authorContent = "";
+      json = json.filter(function(val) {
+        console.log(json);
+        // return (val.author.username === data);
+
+      });
+
+      json.forEach(function(val) {
+        authorContent += '<div class="mdl-card mdl-cell mdl-cell--4" >';
+        authorContent += '<div class="mdl-card__media mdl-color-text--grey-50" style="background-image: url(' + news[i].image + ');">';
+        authorContent += '</div>';
+        authorContent += '<h3><a href="' + news[i].link + '">' + news[i].headline + '</a></h3>';
+        authorContent += '<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">';
+        authorContent += '<div class="minilogo"><a href="http://www.freecodecamp.com/' + news[i].author.username + '"><img class="avatar" src="' + news[i].author.picture + ')"></a></div>';
+        authorContent += '<div>';
+        authorContent += '<a class="author-link" href="http://www.freecodecamp.com/' + news[i].author.username + '"><strong>@' + news[i].author.username + '</strong></a>';
+        authorContent += '<span>' + jQuery.timeago(news[i].timePosted) + '</span>';
+        authorContent += '<span>' + news[i].rank + (news[i].rank > 1 ? " votes" : " vote") + '</span></div>';
+        authorContent += '<button id="' + news[i].id + '" class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--primary mdl-upvote-btn"><i class="material-icons mdl-color-text--white btn" role="presentation">arrow_upward</i><span class="visuallyhidden">arrow_upward</span></button>';
+        authorContent += '</div>';
+        authorContent += '</div>';
+        authorContent += '</div>';
+        authorContent += '</div>';
+      });
+      var content = document.getElementById('news');
+      content.innerHTML = authorContent;
+    });
   });
 
 
-
-});
+}, JQuery);
