92d4c581ad4a89f6e1bb33474d05e8e5db10aee0
libertysyarn <code@libertysyarn.com>
2016-02-01 14:18:48 -0500

    fixed styling

--- js/index.js
+++ js/index.js
@@ -16,14 +16,16 @@ function reqListener() {
 
       newContent += '<div class="mdl-card mdl-cell mdl-cell--4" >';
       newContent += '<div class="mdl-card__media mdl-color-text--grey-50" style="background-image: url(' + news[i].image + ');">';
-      newContent += '<h3><a href="' + news[i].link + '">' + news[i].headline + '</a></h3>';
       newContent += '</div>';
+      newContent += '<h3><a href="' + news[i].link + '">' + news[i].headline + '</a></h3>';
       newContent += '<div class="mdl-card__supporting-text meta mdl-color-text--grey-600">';
       newContent += '<div class="minilogo"><a href="http://www.freecodecamp.com/' + news[i].author.username + '"><img class="avatar" src="' + news[i].author.picture + ')"></a></div>';
       newContent += '<div>';
-      newContent += '<button class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--colored"><i class="material-icons mdl-color-text--white" role="presentation">arrow_upward</i><span class="visuallyhidden">arrow_upward</span></button>'
-      newContent += '<a class="author-link" href="http://www.freecodecamp.com/' + news[i].author.username + '"><strong>' + news[i].author.username + '</strong></a>';
+
+      newContent += '<a class="author-link" href="http://www.freecodecamp.com/' + news[i].author.username + '"><strong>@' + news[i].author.username + '</strong></a>';
       newContent += '<span>' + jQuery.timeago(news[i].timePosted) + '</span>';
+      newContent += '<span>' + news[i].rank + (news[i].rank > 1 ? " points" : " point") + '</span></div>';
+      newContent += '<button class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--primary mdl-upvote-btn"><i class="material-icons mdl-color-text--white" role="presentation">arrow_upward</i><span class="visuallyhidden">arrow_upward</span></button>';
       newContent += '</div>';
       newContent += '</div>';
       newContent += '</div>';
@@ -41,45 +43,26 @@ function reqListener() {
 //ToDo
 // -make default background image if none exists
 // -assign colors of tiles to key words in title
-// -assign tile size depending on title length
 // -add upvote, rank, author and
 // -sort by newest, rank, tag
-// -figure out where to put "mdl-menu--bottom-right"
 
 
 
 
-  $.ready(function($) {
-    $('.ripple-effect').click(function(e) {
-      var rippler = $($this);
 
-      // create .ink element if it doesn't exist
-      if (rippler.find('.ink').length === 0) {
-        rippler.append("<span class='ink'></span>");
-      }
+$.ready(function() {
+  var div = document.createElement('div');
+  var alreadyUpvoted = false;
 
-      var ink = rippler.find('.ink');
-
-      // prevent quick double clicks
-      ink.removeClass('animate');
-
-      // set .ink diametr
-      if (!ink.height() && !ink.width()) {
-        var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
-        ink.css({
-          height: d,
-          width: d
-        });
-      }
-
-      // get click coordinates
-      var x = e.pageX - rippler.offset().left - ink.width() / 2;
-      var y = e.pageY - rippler.offset().top - ink.height() / 2;
-
-      // set .ink position and add class .animate
-      ink.css({
-        top: y + 'px',
-        left: x + 'px'
-      }).addClass('animate');
-    });
+  $(div).find('.mdl-upvote-btn').each(function(idx, btn) {
+    var $btn = $(btn);
+    if (alreadyUpvoted) {
+      $btn.addClass('disabled');
+      $btn.text('upvoted!');
+    }
+    $btn.data('upVotes', news[i].upVotes);
   });
+
+
+
+});
