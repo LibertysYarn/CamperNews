871c62f8d923e2d3a77c2defc9c2ec1bd6841e9e
Liberty Montano <code@libertysyarn.com>
2017-07-01 21:54:19 -0400

    fetch

--- js/index.js
+++ js/index.js
@@ -1,3 +1,77 @@
+function randImg(imgVar) {
+  let imgNum = getRandomInt(1, 33);
+  let urlVar = 'http://libertymontano.com/FCCfiles/CamperNews/img/mat/' + imgNum + '.png';
+
+  let myRegExp = /^https?:\/\/.*\.(?:jpe?g|gif|png)/;
+  if (!myRegExp.test(imgVar)) {
+    return urlVar;
+  } else {
+    return imgVar;
+  }
+
+  $('.nimg').error(function() {
+    $(this).addClass('noImg');
+  });
+
+}
+
+function getRandomInt(min, max) {
+  var random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return random;
+}
+
+let apiRequest1 = fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.freecodecamp.com%2Ffeed&api_key=l2geiepib5t1z0vtapwkilcxrtz3q2gsc0olmm3h&order_by=pubDate&count=20').then((response) => response.json())
+  .then(function(data) {
+    let newMed = data.results;
+    return newMed.map(function(news) {
+      let itemURL = news.items.guid,
+        itemTitle = news.items.title,
+        author = news.items.author,
+        feedLink = news.feed.link,
+        feedImg = news.feed.image,
+        pubDate = news.items.pubDate,
+        category = news.items.categories,
+        descToString = JSON.stringify(news.items.description),
+        jsonImg = descToString.split('"'),
+        img = jsonImg[4],
+        finalImg = randImg(img.slice(0, -1));
+
+    }).catch(function(error) {
+      console.log(error);
+    })
+  });
+
+let apiRequest2 = fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8butISFwT-Wl7EV0hUK0BQ&maxResults=10&order=date&type=video&key=AIzaSyB_pv93cd3WwdCT0AeI92xzC2KM4U9p1Q0').then((response) => response.json())
+  .then(function(data) {
+    let video = data.results;
+    return video.map(function(videos) {
+      let itemURL = 'https://youtube.com/watch?v=' + video.items.videoId;
+       itemTitle = videos.snippet.title,
+       author = videos.snippet.channelTitle,
+      // let feedLink = news.feed.link;
+       feedImg = videos.snippet.image,
+       pubDate = videos.snippet.publishedAt,
+      // let category = news.items.categories;
+       finalImg = randImg(videos.snippet.medium.url);
+
+    }).catch(function(error) {
+      console.log(error);
+    })
+  });
+
+let combinedData = {
+  "apiRequest1": {},
+  "apiRequest2": {}
+};
+Promise.all([apiRequest1, apiRequest2]).then(function(values) {
+  combinedData["apiRequest1"] = values[0];
+  combinedData["apiRequest2"] = values[1];
+  console.log(combinedData);
+  return combinedData;
+});
+
+
+
 let rss = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.freecodecamp.com%2Ffeed&api_key=l2geiepib5t1z0vtapwkilcxrtz3q2gsc0olmm3h&order_by=pubDate&count=20";
 let content = document.getElementById('news');
 
@@ -23,14 +97,11 @@ xhr.onreadystatechange = function() {
         // newContent += '<div>';
         newContent += '<span class="block"><span class="author-container"><a class="author-link author-item" href="http://www.freecodecamp.com/' + news.items[i].author + '">@' + news.items[i].author + '</a>';
         newContent += '<span class="timeago author-it">  /  ' + jQuery.timeago(news.items[i].pubDate) + '</span></div>';
+        (news.items[i].categories).map(e => newContent += '<div class="chip chip-text"><a href="https://medium.com/topic/' + e + '">' + e + '</a></div>');
         newContent += '<i class="material-icons right">close</i></div>';
         // newContent += '<span>' + news.items[i].score + (news.items[i].score > 1 ? " votes" : " vote") + '</span></div>';
         // newContent += '<button id="' + news.items[i].id + '" class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--primary mdl-upvote-btn"><i class="material-icons mdl-color-text--white btn" role="presentation">arrow_upward</i><span class="visuallyhidden">arrow_upward</span></button>';
-        // newContent += '<button id="my-menu-top-right" class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons" role="presentation">more_vert</i></button>';
-        // newContent += '<ul class="mdl-menu mdl-menu--top-right mdl-js-menu mdl-js-ripple-effect" for="my-menu-top-right">';
-        // (news.items[i].categories).map(e =>  newContent += '<li class="mdl-menu__item"><a href="https://medium.com/topic/' + e + '">' + e + '</a></li>');
-        // newContent += '</ul></div></div>';
-        // newContent += '</div>';
+
         newContent += '</div>';
         newContent += '</div>';
         newContent += '</div>';
@@ -38,38 +109,27 @@ xhr.onreadystatechange = function() {
 
       content.innerHTML = newContent;
 
-      function catChips() {
-        let chips = (news.items[i].categories).map(function(e) {
-          console.log(e);
-          return 'newContent += <span class="mdl-chip"><span class="mdl-chip__text">' + e + '</span></span>';
-        })
-      }
-
-      function randImg() {
-        let imgNum = getRandomInt(1, 33);
-        let urlVar = 'http://libertymontano.com/FCCfiles/CamperNews/img/mat/' + imgNum + '.png';
-        let descToString = JSON.stringify(news.items[i].description);
-        let jsonImg = descToString.split('"');
-        let img = jsonImg[4];
-        // console.log(img.slice(0, -1));
-        let myRegExp = /^https?:\/\/.*\.(?:jpe?g|gif|png)/;
-        let urlToValidate = img.slice(0, -1);
-        if (!myRegExp.test(urlToValidate)) {
-          return urlVar;
-        } else {
-          return img.slice(0, -1);
-        }
-
-        $('.nimg').error(function() {
-          $(this).addClass('noImg');
-        });
-
-      }
-
-      function getRandomInt(min, max) {
-        var random = Math.floor(Math.random() * (max - min + 1)) + min;
-        return random;
-      }
+      // function randImg() {
+      //   let imgNum = getRandomInt(1, 33);
+      //   let urlVar = 'http://libertymontano.com/FCCfiles/CamperNews/img/mat/' + imgNum + '.png';
+      //
+      //   let myRegExp = /^https?:\/\/.*\.(?:jpe?g|gif|png)/;
+      //   if (!myRegExp.test(urlToValidate)) {
+      //     return urlVar;
+      //   } else {
+      //     return urlToValidate;
+      //   }
+      //
+      //   $('.nimg').error(function() {
+      //     $(this).addClass('noImg');
+      //   });
+      //
+      // }
+      //
+      // function getRandomInt(min, max) {
+      //   var random = Math.floor(Math.random() * (max - min + 1)) + min;
+      //   return random;
+      // }
 
     }
   }
@@ -78,21 +138,13 @@ xhr.open('GET', rss, true);
 xhr.send();
 
 $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true, // Choose whether you can drag to open on touch screens,
-      // onOpen: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is opened
-      // onClose: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is closed
-    }
-  );
+  menuWidth: 300, // Default is 300
+  edge: 'left', // Choose the horizontal origin
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true, // Choose whether you can drag to open on touch screens
+});
 
 
-// function authListener(id) {
-//     var byUserId = JSON.search(news, '//*[contains(userId, "id")]');
-//     var htm = Defiant.render('author_template', byUserId);
-//     document.getElementById('news').innerHTML = htm;
-// }
 
 
 
